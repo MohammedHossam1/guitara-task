3ef6987cd75e18d1dd8b1f8d21c9b5f7693dbf44
mohammedhossam1 <mohammedhossam199998@gmail.com>
2025-08-12 04:03:16 +0200

    fixed footer newsletter

--- src/components/layout/footer/news-letter.tsx
+++ src/components/layout/footer/news-letter.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 const NewsLetter = () => {
     return (
         <div className=" " >
-            <h3 className=" text-[#9F9F9F] font-medium mb-3 lg:mb-10">Newsletter</h3>
+            <h3 className=" text-[#9F9F9F] font-medium mb-3 lg:mb-10 ">Newsletter</h3>
             <form className="flex max-lg:flex-col gap-2">
                 <input
                     type="email"
                     required
                     placeholder="Enter Your Email Address"
-                    className="border-b border-gray-400 outline-none flex-1 py-1 text-sm"
+                    className="border-b border-gray-400  w-10/12 py-1 text-sm"
                 />
                 <button
                     type="submit"
